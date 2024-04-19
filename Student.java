6a83b98e7692334017c23fc8772390e400dc5daa
HemachandarK <138129846+HemachandarK@users.noreply.github.com>
2024-04-19 22:34:17 +0530

    week8-day3(standalone, stereotype,spel)

--- Student.java
+++ Student.java
@@ -1,23 +1,48 @@
-package com.springcore;
+package com.springcore.stereotype;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component("ob")
+@Scope("prototype")
 public class Student {
-	private int rno;
-	private String name;
-	public int getRno() {
-		return rno;
+	
+	@Value("Durgesh Tiwari")
+	private String studentName;
+	@Value("Lucknow")	
+	private String city;
+	
+	@Value("#{temp}")
+	private List<String> address;
+	
+	public String getStudentName() {
+		return studentName;
 	}
-	public void setRno(int rno) {
-		this.rno = rno;
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
 	}
-	public String getName() {
-		return name;
+	public String getCity() {
+		return city;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setCity(String city) {
+		this.city = city;
 	}
+	
+	
+	
+	public List<String> getAddress() {
+		return address;
+	}
+	public void setAddress(List<String> address) {
+		this.address = address;
+	}
+	@Override
 	public String toString() {
-	return "Studend [studentId=" + rno + ", studentName=" + name 
-	+ "]";
-}
-
+		return "Student [studentName=" + studentName + ", city=" + city + "]";
+	}
+	
+	
 }
--- Test.java
+++ Test.java
@@ -1,4 +1,4 @@
-package com.springcore.auto.wire.annotation;
+package com.springcore.stereotype;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -6,10 +6,24 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/auto/wire/annotation/autoconfig.xml");
-		Emp emp1 = context.getBean("emp1", Emp.class);
-		System.out.println(emp1);
+		ApplicationContext con = new ClassPathXmlApplicationContext("com/springcore/stereotype/stereoconfig.xml");
+		Student student = con.getBean("ob", Student.class);
+//		System.out.println(student);
+//		System.out.println(student.getAddress());
+//		System.out.println(student.getAddress().getClass().getName());
+		
+//		System.out.println(student.hashCode());
+		
+		Student student2 = con.getBean("ob",Student.class);
+//		System.out.println(student2.hashCode());
+		
+		Teacher t1 = con.getBean("teacher",Teacher.class);
+		Teacher t2 = con.getBean("teacher",Teacher.class);
+		
+		System.out.println(t1.hashCode());
+		System.out.println(t2.hashCode());
 	}
 
 }
